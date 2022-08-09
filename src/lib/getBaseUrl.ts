f45de9ae15caf3bd5b590f8a19458e1fa7fba3c8
Herbie Vine <vineherbie@gmail.com>
2022-08-09 10:03:14 +0200

    trying to fix seo issues 3

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,6 +1,7 @@
 const getBaseUrl = (): string => {
-  const { origin } = window.location;
-  return process.env.NEXT_PUBLIC_URL ?? origin;
+  return process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
 export default getBaseUrl;
