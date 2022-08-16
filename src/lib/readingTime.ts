9e5930857a7be738505bc0dbe876c9cc886953e3
Herbie Vine <vineherbie@gmail.com>
2022-08-16 15:29:22 +0200

    init

--- src/lib/readingTime.ts
+++
@@ -1,8 +0,0 @@
-const readingTime = (text: string): string => {
-  const wordsPerMinute = 200;
-  const words = text.split(/\s+/).length;
-  const minutes = Math.round(words / wordsPerMinute);
-  return `${minutes} min read`;
-};
-
-export default readingTime;
--- src/schema/post.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-import { metadataSchema } from "./metadata";
-
-export const postSchema = z.object({
-  content: z.string(),
-  metadata: metadataSchema,
-});
-
-export type TPost = z.infer<typeof postSchema>;
