ed33344d44be59fd998437ce6241a1975306421a
Herbie Vine <vineherbie@gmail.com>
2023-04-15 19:16:33 +0200

    edit desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,10 @@ export const metadata: Metadata = {
     default: "Reactive",
     template: "%s - Reactive",
   },
-  description: "A blog about React, Next.js, and TypeScript",
+  description: "A blog about programming concepts and technologies",
   openGraph: {
     title: "Reactive",
-    description: "A blog about React, Next.js, and TypeScript",
+    description: "A blog about programming concepts and technologies",
     type: "website",
     url: "https://reactive.so",
     images: [
