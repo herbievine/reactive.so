56fdb47d1b6bf5ddd78af12e38e1009475062b86
Herbie Vine <me@herbievine.com>
2024-08-21 09:16:48 +0200

    chore: change domain again for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://reactive-so.pages.dev",
+  site: "https://reactive.so",
   integrations: [mdx(), sitemap(), tailwind()],
 });
