1e57d3ff161d602286d3044f7510e391f4d9d0dc
Herbie Vine <me@herbievine.com>
2024-08-21 09:05:22 +0200

    chore: change url and remove rss + react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://reactive.so",
+  site: "https://https://reactive-so.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
 });
