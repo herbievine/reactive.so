639828a246809d7335b5b6fedf9fadd3dea96a9e
Herbie Vine <me@herbievine.com>
2025-05-29 10:30:09 +0200

    fix: trailing slack false -> never

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://reactive.so",
   integrations: [mdx(), sitemap(), tailwind()],
-  trailingSlash: false
+  trailingSlash: "never",
 });
